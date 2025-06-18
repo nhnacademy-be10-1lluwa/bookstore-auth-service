409b9b7715f0900f7e42eb7f5536793eecd245f9
pear-c <100738513+pear-c@users.noreply.github.com>
2025-06-18 13:45:39 +0900

    chore: auth-service 프로젝트 기본 구조 세팅 (#2)

--- src/main/java/com/nhnacademy/illuwa/AuthServiceApplication.java
+++ src/main/java/com/nhnacademy/illuwa/AuthServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IlluwaApplication {
+public class AuthServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IlluwaApplication.class, args);
+        SpringApplication.run(AuthServiceApplication.class, args);
     }
 
 }
