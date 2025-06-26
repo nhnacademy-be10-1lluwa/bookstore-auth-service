5a5ecff5e6a48c9f2c165b12c08a3a994cefda80
pear_c <zxcv9676@naver.com>
2025-06-26 17:45:35 +0900

    feat: 회원가입 api 작성 user service api 연동

--- src/main/java/com/nhnacademy/illuwa/dto/TokenResponse.java
+++ src/main/java/com/nhnacademy/illuwa/dto/TokenResponse.java
@@ -1,4 +1,4 @@
-package com.nhnacademy.illuwa.domain.auth.dto;
+package com.nhnacademy.illuwa.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
---
+++ src/main/java/com/nhnacademy/illuwa/service/AuthService.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.illuwa.service;
+
+import com.nhnacademy.illuwa.dto.SignupRequest;
+
+public interface AuthService {
+    void signup(SignupRequest signupRequest);
+//    String login(LoginRequest request);
+}
